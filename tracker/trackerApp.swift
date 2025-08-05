cd2904bde671006fec8b0df1a2aa820e7d84930c
pavelgaiduk <pavel.gaiduk@hwschool.online>
2025-08-05 16:48:41 +0300

    Initial Commit

---
+++ tracker/trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  trackerApp.swift
+//  tracker
+//
+//  Created by Work on 8/5/25.
+//
+
+import SwiftUI
+
+@main
+struct trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ trackerTests/trackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  trackerTests.swift
+//  trackerTests
+//
+//  Created by Work on 8/5/25.
+//
+
+import Testing
+@testable import tracker
+
+struct trackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
